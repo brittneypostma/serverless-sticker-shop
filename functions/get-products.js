df6d99395d4f1c8e1167e41cf6dbedfb50ffa267
Brittney <brittney.postma@hotmail.com>
2020-08-10 16:00:20 -0400

    prices data being pulled

--- functions/get-products.js
+++ functions/get-products.js
@@ -1,16 +1,15 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
-exports.handler = (event, context, callback) => {
-  stripe.products.list((err, products) => {
-    if (err) {
-      callback(err)
-    }
-    else {
-      callback(null,
-        {
-          statusCode: 200,
-          body: JSON.stringify(products),
-        })
-    }
-  })
+exports.handler = async (event, context, callback) => {
+  try {
+    const prices = await stripe.prices.list({ expand: ["data.product"] })
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(prices)
+    })
+  }
+  catch {
+    console.log("Something went wrong.")
+  }
 }
+
--- public/js/load-products.js
+++ public/js/load-products.js
@@ -1,3 +1,31 @@
+async function handleFormSubmit(e) {
+  e.preventDefault()
+  const form = new FormData(e.target)
+
+  const data = {
+    sku: form.get('sku'), quantity: 1
+  }
+
+  const res = await fetch('/.netlify/functions/create-checkout', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }).then(res => res.json)
+
+  const stripe = Stripe(res.publishableKey)
+
+  const { error } = await stripe.redirectToCheckout({
+    sessionId: res.sessionId
+  })
+
+  if (error) {
+    console.error(error)
+  }
+}
+
+
 function createTemplate(item) {
   const template = document.querySelector('#product')
   const product = template.content.cloneNode(true)
@@ -10,6 +38,9 @@ function createTemplate(item) {
   product.querySelector('.description').innerText = item.description
   product.querySelector('[name=sku]').value = item.id
 
+  const form = product.querySelector('form')
+  form.addEventListener('submit', handleFormSubmit)
+
   return product
 }
 
@@ -17,8 +48,8 @@ export async function loadProducts() {
   const data = await fetch('/.netlify/functions/get-products')
     .then(res => res.json())
     .catch(err => console.error(err))
-
-  data.data.forEach(product => {
-    document.body.appendChild(createTemplate(product))
-  })
+  console.log(data)
+  // data.data.forEach(product => {
+  //   document.body.appendChild(createTemplate(product))
+  // })
 }
